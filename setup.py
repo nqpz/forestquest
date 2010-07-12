8284d5027485a45563aeb95c2bfabb46d9eed476
Niels Serup <ns@metanohi.org>
2010-07-12 18:07:08 +0200

    Will this be the 0.1 release?

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ ginfo_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                           'forestquest', 'generalinformation.py')
 execfile(ginfo_file) # Gives us a version variable
 
+data = []
+for x in os.walk(os.path.join('forestquest', 'data')):
+    data.append((os.path.join('share', x[0]),
+                 [os.path.join(x[0], y) for y in x[2]]))
+
 setup(
     name='ForestQuest',
     version=version,
     author='Niels Serup',
     author_email='ns@metanohi.org',
     packages=['forestquest'],
+    data_files = data,
     url='http://metanohi.org/projects/forestquest/',
     license='LICENSING.txt',
     description='An exiting RPG',
